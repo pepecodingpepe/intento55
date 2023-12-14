3d73a936f659a1e9fa1a9c98ef758d244915310f
pepecodingpepe <brokentelevisionbroken@gmail.com>
2023-12-14 05:34:16 -0700

    ya nomas el contact

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ const Home = () => {
 
               <figure class="relative h-auto max-w-3xl transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
         <a href="/contact">
-          <img class="rounded-lg" src="https://www.druidai.com/hubfs/Imported_Blog_Media/business-process-automation-chatbot.jpg" alt="image description"/>
+          <img class="rounded-lg" src="https://ici.net.au/blog/wp-content/uploads/2018/04/ICI-Jobs-for-People-with-Creative-Minds.jpg" alt="image description"/>
           </a>
            <figcaption class="absolute px-4 text-2xl text-white bottom-6">
             <p className="font-mono items-center">Hagamos crecer tu negocio</p>
@@ -49,20 +49,20 @@ const Home = () => {
   
       
       <div className=" flex flex-wrap justify-center p-8 mx-0.5 mx-3.5">
-            <div className="mx-7 w-96 "> <ServiceCard imageUrl="https://www.druidai.com/hubfs/Imported_Blog_Media/business-process-automation-chatbot.jpg" 
+            <div className="mx-7 w-96 "> <ServiceCard imageUrl="https://assets-prd.ignimgs.com/2021/12/16/sale-309437-article-image-1639680628904.jpeg" 
             title="Diseño gráfico en tiempo real" 
             description="Servicio para quienes tienen una idea clara y precisa de lo que necesitan diseñar, desde un logotipo, hasta una etiqueta o un poster para un evento. " 
             ctaText="Mas información" ctaLink="/services" /></div>
 
-            <div className="mx-7 w-96"> <ServiceCard imageUrl="https://www.druidai.com/hubfs/Imported_Blog_Media/business-process-automation-chatbot.jpg" 
+            <div className="mx-7 w-96"> <ServiceCard imageUrl="https://www.cetys.mx/noticias/wp-content/uploads/2022/04/Foto-1.jpg" 
             title="Consultoría 1 a 1 para emprendedores" 
             description="Si lo que necesitas es atención personalizada para atender tus necesidades específicas, este servicio es para tí." ctaText="Mas información" ctaLink="/services" /></div>
 
-            <div className="mx-7 w-96"> <ServiceCard imageUrl="https://www.druidai.com/hubfs/Imported_Blog_Media/business-process-automation-chatbot.jpg" 
+            <div className="mx-7 w-96"> <ServiceCard imageUrl="https://tanyadigital.com/wp-content/uploads/2022/11/sales-growth-adalah.jpg" 
             title="Estrategias de expansión comercial" 
             description="Incrementa las ventas de tu negocio con los recursos que tienes a la mano, nosotros nos encargamos de armar una estrategia a tu medida." ctaText="Mas información" ctaLink="/services" /></div>
 
-            <div className="mx-7 w-96"> <ServiceCard imageUrl="https://www.druidai.com/hubfs/Imported_Blog_Media/business-process-automation-chatbot.jpg" 
+            <div className="mx-7 w-96"> <ServiceCard imageUrl="https://www.highdigital.co.uk/wp-content/uploads/2022/02/iStock-915188574.jpg" 
             title="Modelador de negocios: comienza desde cero" 
             description="¿Estas a punto de abandonar tu trabajo fijo para comenzar un negocio? Te ayudamos en cada paso para que lo logres sin contratiempos" ctaText="Mas información" 
             ctaLink="/services" /></div>
