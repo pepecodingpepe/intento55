517dcc62ddfd9b42661cae1590c8eb1a29809d3a
pepecodingpepe <brokentelevisionbroken@gmail.com>
2023-12-14 16:33:22 -0600

    a vers

---
+++ pages/nada.js
@@ -0,0 +1,25 @@
+
+import Header from '../components/common/Header';
+import CustomHead from '../components/common/CustomHead'; // Importa el componente CustomHead
+
+import Footer from '../components/common/Footer';
+
+const Clients = () => {
+  return (
+    <div>
+      <CustomHead />
+      <Header />
+      <section className="min-h-screen flex items-center justify-center">
+      <p className="flex-wrap font-serif text-7xl text-center font-bold mb-2 py-10 mx-44">Página en construcción </p>
+      
+
+ 
+      </section>
+      
+      <Footer />
+    </div>
+  );
+};
+
+export default Clients;
+
