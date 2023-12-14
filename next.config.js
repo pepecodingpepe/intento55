0b66dce19644ad94d50293bbbd9c75f6c2d3301f
pepecodingpepe <brokentelevisionbroken@gmail.com>
2023-12-14 16:55:59 -0600

    vaa

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
+ module.exports = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
