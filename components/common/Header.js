92cba4b24148905be9733e1f14cb94540d2da422
pepecodingpepe <brokentelevisionbroken@gmail.com>
2024-05-20 02:10:25 -0600

    Hard Fix en versión de next , gracias al nuevo header

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,6 +1,244 @@
 // /components/common/Header.js
+
+
+
+
+import { Fragment, useState } from 'react'
+import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+import {
+  Bars3Icon,
+  ChartPieIcon,
+  CommandLineIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  LightBulbIcon,
+  PaintBrushIcon,
+  RocketLaunchIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon, RectangleGroupIcon } from '@heroicons/react/20/solid'
+
+const products = [
+  {
+    name: 'Soluciones Visuales',
+    description: 'Eleva tu marca o proyecto al siguiente nivel con nuestros diferentes servicios de diseño gráfico profesional. ',
+    href: '#',
+    icon: PaintBrushIcon,
+  },
+  {
+    name: 'Consultoría Estratégica',
+    description: 'Desde crear una campaña publicitaria para atraer nuevos clientes, hasta el diseño de tu sistema integral de trabajo.',
+    href: '#',
+    icon: RocketLaunchIcon,
+  },
+  { name: 'Desarrollo Web', description: 'Posicionate en línea y accede a nuevas oportunidades de negocio con tu sitio web personalizado.', href: '#', icon: CommandLineIcon },
+  {
+    name: 'Modelador de Negocios',
+    description: 'Te ayudamos a planificar, estructurar y lanzar tu nuevo proyecto. Somos una comunidad local de emprendedores independientes.',
+    href: '#',
+    icon: LightBulbIcon,
+  },
+]
+const callsToAction = [
+  { name: 'Dopamina AudioVisual', href: '#', icon: PlayCircleIcon },
+  { name: 'Agenda tu consulta inicial gratis', href: '#', icon: PhoneIcon },
+  { name: 'Ver todos los servicios', href: '#', icon: RectangleGroupIcon },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <header className="relative isolate z-10 bg-gradient-to-r from-slate-100 to-slate-50 shadow-md">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1 p-1">
+            <span className="sr-only">Dopamina Visual</span>
+            <img className="h-16 w-auto" src="https://i.postimg.cc/7hFSPtPF/Material-Didactico-02-02.png" alt="" />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+          <Popover>
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+              Servicios
+              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 -translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 -translate-y-1"
+            >
+              <Popover.Panel className="absolute inset-x-0 top-0 -z-10 bg-white pt-14 shadow-lg ring-1 ring-gray-900/5">
+                <div className="mx-auto grid max-w-7xl grid-cols-4 gap-x-4 px-6 py-10 lg:px-8 xl:gap-x-8">
+                  {products.map((item) => (
+                    <div key={item.name} className="group relative rounded-lg p-6 text-sm leading-6 hover:bg-gray-50">
+                      <div className="flex h-11 w-11 items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
+                      </div>
+                      <a href={item.href} className="mt-6 block font-semibold text-gray-900">
+                        {item.name}
+                        <span className="absolute inset-0" />
+                      </a>
+                      <p className="mt-1 text-gray-600">{item.description}</p>
+                    </div>
+                  ))}
+                </div>
+                <div className="bg-gray-50">
+                  <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="grid grid-cols-3 divide-x divide-gray-900/5 border-x border-gray-900/5">
+                      {callsToAction.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
+                        >
+                          <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          Portafolio
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          Nuestros Clientes
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          Árbol de Vida
+          </a>
+        </Popover.Group>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Contacto <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </nav>
+      <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Dopamina Visual</span>
+              <img
+                className="h-8 w-auto"
+                src="https://i.postimg.cc/7hFSPtPF/Material-Didactico-02-02.png"
+                alt=""
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Cerrar menú</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                        Servicios
+                        <ChevronDownIcon
+                          className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {[...products, ...callsToAction].map((item) => (
+                          <Disclosure.Button
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                          >
+                            {item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Portafolio
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Nuestros Clientes
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Árbol de Vida
+                </a>
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Contacto
+                </a>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  )
+}
+
+
+
+
+
+
+
+
+
+/*{  Header Viejo _______________________________________
+
+
 import Link from 'next/link';
 import { useState } from 'react';
+
+
+
 
 const Header = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -40,3 +278,12 @@ const Header = () => {
 };
 
 export default Header;
+
+
+
+
+}*/
+
+
+
+
